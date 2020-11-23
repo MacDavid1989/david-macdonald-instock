11181f3fafe2fe0b6bb47a49e0f32ff88bfba779
David <62397679+MacDavid1989@users.noreply.github.com>
2020-11-23 16:43:14 -0500

    Require warehouseIdRoute in server.js. Use '/:warehouseId' forthat route. Set up router.get for warehouseIdRoute and create const data variables for the warehouses and inventories using fs readFileSync.

--- server/routes/warehouseIdRoute.js
+++ server/routes/warehouseIdRoute.js
@@ -1,7 +1,14 @@
 const fs = require('fs')
 const express = require('express');
 const router = express.Router();
-const warehouses = '../data/warehouses.json';
-const inventories = '../data/inventories.json'
+const warehouses = './data/warehouses.json';
+const inventories = './data/inventories.json'
+
+// GET /:warehouseId
+router.get('/', (req,res) => {
+    const warehousesData = fs.readFileSync(warehouses);
+    const inventoriesData = fs.readFileSync(inventories);
+
+} )
 
 module.exports = router;
--- server/server.js
+++ server/server.js
@@ -1,13 +1,20 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const warehouseIdRoute = require('./routes/warehouseIdRoute')
 
+// allows use of .env
 require('dotenv').config();
 const port = process.env.PORT
 
+// request body middleware
 app.use(express.json());
 
+// allows cors
 app.use(cors());
+
+// warehouse id route
+app.use('/:warehouseId', warehouseIdRoute)
 
 app.listen(port, ()=>{
     console.log(`Server Started on http://localhost:${port}`);
