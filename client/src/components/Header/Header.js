12b2253481814d74d1b524d0d750753403c5d0e4
Joseph Abate <Joseph_Abate_@hotmail.com>
2020-11-23 17:00:51 -0500

    header mobile scss

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -1,11 +1,17 @@
 import React from 'react';
-import './Header.css';
+import './header.css';
+
+import logo from '../../Assets/Logo/InStock-Logo_1x.png'
 
 const header = () => {
     return (
-        <div>
-            <h1>Header</h1>
-        </div>
+        <header className="header">
+            <img src={logo} alt="logo" />
+            <div>
+                <button className="header__btn header__btn--active">Warehouses</button>
+                <button className="header__btn">Inventory</button>
+            </div>
+        </header>
     );
 }
 
