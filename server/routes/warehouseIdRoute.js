842ed5e384df6e04a5c242b8ff1d8004d1b38993
David <62397679+MacDavid1989@users.noreply.github.com>
2020-11-23 16:33:32 -0500

    Create const variables for fs, express, router, warehouses file and inventories file. Export router.

---
+++ server/routes/warehouseIdRoute.js
@@ -0,0 +1,7 @@
+const fs = require('fs')
+const express = require('express');
+const router = express.Router();
+const warehouses = '../data/warehouses.json';
+const inventories = '../data/inventories.json'
+
+module.exports = router;
