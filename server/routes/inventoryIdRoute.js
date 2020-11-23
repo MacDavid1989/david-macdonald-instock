060aea0c42848be02fcf31aa24ed75c6beff7d58
David <62397679+MacDavid1989@users.noreply.github.com>
2020-11-23 17:56:45 -0500

    Create inventory route to return a specified inventory item based on inventory id.

---
+++ server/routes/inventoryIdRoute.js
@@ -0,0 +1,22 @@
+const fs = require('fs')
+const express = require('express');
+const router = express.Router();
+const inventories = './data/inventories.json'
+
+// GET /:warehouseId/inventory/:inventoryId
+router.get('/:inventoryId', (req,res) => {
+    const inventoriesData = fs.readFileSync(inventories);
+
+    const inventoriesDataParsed = JSON.parse(inventoriesData);
+
+    const inventoryId = req.params.inventoryId;
+
+    const inventoryItem = inventoriesDataParsed.filter(inventoryItem => inventoryItem.id === inventoryId)
+
+    if(inventoryItem[0] === undefined){
+        return res.status(404).json('Item not found.')
+    }
+    return res.status(200).json({inventoryItem: inventoryItem})
+} )
+
+module.exports = router;
