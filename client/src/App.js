4f9d65b7fcc5b47be1a932cad2d562d6c89c8527
Jasper Leung <JasperLeung@Jaspers-MacBook-Pro.local>
2020-11-23 22:26:07 -0500

    TOR20Q1NK-3, Front-End: React Router Setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter, Switch, Route} from 'react-router-dom';
+import Home from './pages/Home/Home';
 
 function App() {
   return (
-    <div className="App">
-    </div>
+    <BrowserRouter>
+      <Switch>
+        <Route path="/" exact component={Home}/>
+        {/* <Route path="/warehouse/:warehouseid" component={Warehouse} /> */}
+        {/* <Route path="/warehouse/edit/:warehouseid" component={WarehouseEdit} /> */}
+        {/* <Route path="/warehouse/add" component={WarehouseAdd} /> */}
+        {/* <Route path="/warehouse/inventory/:warehouseid" component={WarehouseInventory} /> */}
+        {/* <Route path="/inventory" component={Inventory} /> */}
+        {/* <Route path="/inventory/:itemid" component={ItemDetails} /> */}
+        {/* <Route path="/inventory/edit/:itemid" component={ItemEdit} /> */}
+        {/* <Route path="/inventory/add" component={ItemAdd} /> */}
+      </Switch>
+    </BrowserRouter>
   );
 }
 
