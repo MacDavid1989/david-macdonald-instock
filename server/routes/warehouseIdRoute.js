5f553596db354d0c28911a0e5d392bffab4e580b
Joseph Abate <Joseph_Abate_@hotmail.com>
2020-11-24 13:01:37 -0500

    fixed newwarehouse method

--- server/routes/warehouseIdRoute.js
+++ server/routes/warehouseIdRoute.js
@@ -1,67 +1,81 @@
-const fs = require('fs')
-const express = require('express');
+const fs = require("fs");
+const express = require("express");
+const { v4: uuidv4 } = require("uuid");
 const router = express.Router();
-const warehouseFile = './data/warehouses.json';
-const inventories = './data/inventories.json'
+const warehouseFile = "./data/warehouses.json";
+const inventories = "./data/inventories.json";
 
 // GET /:warehouseId
-router.get('/:warehouseId', (req,res) => {
-    const warehousesData = fs.readFileSync(warehouseFile);
-    const inventoriesData = fs.readFileSync(inventories);
+router.get("/:warehouseId", (req, res) => {
+  const warehousesData = fs.readFileSync(warehouseFile);
+  const inventoriesData = fs.readFileSync(inventories);
 
-    const warehousesDataParsed = JSON.parse(warehousesData);
-    const inventoriesDataParsed = JSON.parse(inventoriesData);
+  const warehousesDataParsed = JSON.parse(warehousesData);
+  const inventoriesDataParsed = JSON.parse(inventoriesData);
 
-    const warehouseId = req.params.warehouseId;
+  const warehouseId = req.params.warehouseId;
 
-    const warehouse = warehousesDataParsed.find(warehouse => warehouse.id === warehouseId)
-    const inventory = inventoriesDataParsed.filter(inventoryItem => inventoryItem.warehouseID === warehouseId)
+  const warehouse = warehousesDataParsed.find(
+    (warehouse) => warehouse.id === warehouseId
+  );
+  const inventory = inventoriesDataParsed.filter(
+    (inventoryItem) => inventoryItem.warehouseID === warehouseId
+  );
 
-    if(warehouse === undefined){
-        return res.status(404).json('Warehouse not found.')
-    }
-    return res.status(200).json({warehouse: warehouse, inventory: inventory})
-} )
+  if (warehouse === undefined) {
+    return res.status(404).json("Warehouse not found.");
+  }
+  return res.status(200).json({ warehouse: warehouse, inventory: inventory });
+});
 
 // POST / CREATE A NEW WAREHOUSE
-router.post('/create', (req, res) => {
-    //get the list of warehouses
-    const warehouseList = getAllWarehouses();
+router.post("/create", (req, res) => {
+  //get the list of warehouses
+  const warehouseList = getAllWarehouses();
 
-    //make the new warehouse
-    const newWarehouse = createNewWarehouse(req.body);
+  //make the new warehouse
+  const newWarehouse = createNewWarehouse(req.body);
 
-    //add the new warehouse to the list
-    warehouseList.push(newWarehouse);
+  //add the new warehouse to the list
+  warehouseList.push(newWarehouse);
 
-    //update the file
-    fs.writeFileSync(warehouseFile, JSON.stringify(warehouseList));
+  //update the file
+  fs.writeFileSync(warehouseFile, JSON.stringify(warehouseList));
 
-    //send response
-    res.status(200).josn({warehouses: warehouseList});
-})
-
+  //send response
+  res.status(200).josn({ warehouses: warehouseList });
+});
 
 //Method to get all warehouses from JSON FILE
-function getAllWarehouses(){
-    const warehousesData = fs.readFileSync(warehouseFile);
-    return JSON.parse(warehousesData);
+function getAllWarehouses() {
+  const warehousesData = fs.readFileSync(warehouseFile);
+  return JSON.parse(warehousesData);
 }
 
-function createNewWarehouse({name, address, city, country, contactName, position, phoneNumber, email}){
-    const newWarehouse = {
-        "name": name, 
-        "address": address, 
-        "city": city, 
-        "country": country, 
-        "contactName": contactName, 
-        "position": position, 
-        "phoneNumber": phoneNumber, 
-        "email": email
-    }
-
-    return newWarehouse;
+function createNewWarehouse({
+  name,
+  address,
+  city,
+  country,
+  contactName,
+  position,
+  phoneNumber,
+  email,
+}) {
+  const newWarehouse = {
+    id: uuidv4(),
+    name: name,
+    address: address,
+    city: city,
+    country: country,
+    contact: {
+      name: contactName,
+      position: position,
+      phone: phoneNumber,
+      email: email,
+    },
+  };
+  return newWarehouse;
 }
-
 
 module.exports = router;
