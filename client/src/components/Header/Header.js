a53b6e69158290441237864890927757198eccaa
Joseph Abate <Joseph_Abate_@hotmail.com>
2020-11-24 12:25:01 -0500

    updated header css added empty tempalte for home

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -1,18 +1,18 @@
-import React from 'react';
-import './Header.scss';
+import React from "react";
+import "./Header.scss";
 
-import logo from '../../assets/Logo/InStock-Logo_1x.png'
+import logo from "../../assets/Logo/InStock-Logo_1x.png";
 
 const header = () => {
-    return (
-        <header className="header">
-            <img src={logo} alt="logo" />
-            <div>
-                <button className="header__btn header__btn--active">Warehouses</button>
-                <button className="header__btn">Inventory</button>
-            </div>
-        </header>
-    );
-}
+  return (
+    <header className="header">
+        <img className="header__logo" src={logo} alt="logo" />
+      <div className="header__wrapper">
+        <button className="header__btn header__btn--active">Warehouses</button>
+        <button className="header__btn">Inventory</button>
+      </div>
+    </header>
+  );
+};
 
 export default header;
