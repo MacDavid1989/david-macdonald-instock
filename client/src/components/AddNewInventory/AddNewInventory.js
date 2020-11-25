afe309fbf69535c6ea70d7e2e43530384535940b
Joseph Abate <Joseph_Abate_@hotmail.com>
2020-11-25 16:45:19 -0500

    fixed body paddings

--- client/src/components/AddNewInventory/AddNewInventory.js
+++ client/src/components/AddNewInventory/AddNewInventory.js
@@ -1,11 +1,16 @@
 import React from 'react';
+
 import AddNewItemHeader from '../AddNewItemHeader/AddNewItemHeader';
+import ItemForm from '../ItemForm/ItemForm';
+
 import './AddNewInventory.scss';
+
 
 const AddNewInventory = () => {
     return (
         <div className="new-inventory">
             <AddNewItemHeader />
+            <ItemForm />
         </div>
     );
 }
---
+++ client/src/components/ItemForm/ItemForm.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+import './ItemForm.scss';
+
+class ItemForm extends Component {
+    render() {
+        return (
+            <div>
+                <h3>Item Detials</h3>
+            </div>
+        );
+    }
+}
+
+export default ItemForm;
