28243b504ebf9c6b694e6c4aab5530d70c0d327b
Joseph Abate <Joseph_Abate_@hotmail.com>
2020-11-25 17:08:13 -0500

    added rest of form tags

--- client/src/components/ItemForm/ItemForm.js
+++ client/src/components/ItemForm/ItemForm.js
@@ -15,29 +15,58 @@ class ItemForm extends Component {
   render() {
     return (
       <div className="item-form-com">
-        <h3 className="item-form-com__title">Item Detials</h3>
-
         <form>
           <div>
-            <label for="name">item Name</label>
-            <input type="text" name="name" />
-          </div>
+            <h3 className="item-form-com__title">Item Detials</h3>
+            <div>
+              <label for="name">item Name</label>
+              <input type="text" name="name" />
+            </div>
 
-          <div>
-            <label for="name">Description</label>
-            <textarea></textarea>
-          </div>
+            <div>
+              <label for="name">Description</label>
+              <textarea></textarea>
+            </div>
 
+            <div>
+              <label for="category">Category</label>
+              <select name="category">
+                <option defaultValue hidden>
+                  Pick A Category
+                </option>
+                <option value="Accessories">Accessories</option>
+                <option value="Apparel">Apparel</option>
+                <option value="Electronics">Electronics</option>
+                <option value="Gear">Gear</option>
+                <option value="Health">Health</option>
+              </select>
+            </div>
+          </div>
           <div>
-            <label name="category">Category</label>
-            <select>
-              <option defaultValue hidden>Pick A Category</option>
-              <option value="Accessories">Accessories</option>
-              <option value="Apparel">Apparel</option>
-              <option value="Electronics">Electronics</option>
-              <option value="Gear">Gear</option>
-              <option value="Health">Health</option>
-            </select>
+            <h3 className="item-form-com__title">Item Availability</h3>
+            <div>
+                <p>Status</p>
+                <input type="radio" name="status" value={true}/>
+                <label>In Stock</label>
+                <input type="radio" name="status" value={false}/>
+                <label>Out of Stock</label>
+            </div>
+            <div>
+                <label for="qty">Quantity</label>
+                <input type="number" name="qty" />
+            </div>
+            <div>
+              <label name="warehouse">Warehouse</label>
+              <select name="Warehouse">
+                <option defaultValue hidden>
+                  Please Select
+                </option>
+              </select>
+            </div>
+          </div>
+          <div>
+              <button type="reset">Cancel</button>
+              <button type="submit"> + Add Item</button>
           </div>
         </form>
       </div>
