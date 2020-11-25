fe0cdf80905470bc28c5d4275dbefb46b407b219
Jasper Leung <JasperLeung@Jaspers-MacBook-Pro.local>
2020-11-24 22:28:42 -0500

    TOR20Q1NK-21 Back-End: API to GET List of all Inventory Items

--- server/routes/inventoryRoute.js
+++ server/routes/inventoryRoute.js
@@ -1,11 +1,10 @@
 const fs = require('fs')
 const express = require('express');
 const router = express.Router();
-const inventories = './data/inventories.json'
 
 // GET / all inventory items
 const readFileSync = () => {
-    return JSON.parse(fs.readFileSync(inventories))
+    return JSON.parse(fs.readFileSync('./data/inventories.json'))
 }
 
 router.get("/", (req, res) => {
