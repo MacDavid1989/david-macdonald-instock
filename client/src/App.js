8d4b2a02c5264f431370bedaefd026778efc6990
Joseph Abate <Joseph_Abate_@hotmail.com>
2020-11-25 16:36:46 -0500

    fixed background colors

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,28 @@
-import './App.css';
-import {BrowserRouter, Switch, Route} from 'react-router-dom';
-import Home from './pages/Home/Home';
-import Header from './components/Header/Header';
-import AddnewInventory from './components/AddNewInventory/AddNewInventory';
+import "./App.scss";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import Home from "./pages/Home/Home";
+import Header from "./components/Header/Header";
+import AddnewInventory from "./components/AddNewInventory/AddNewInventory";
 
 function App() {
   return (
     <BrowserRouter>
-    <Header />
-      <Switch>
-        <Route path="/" exact component={Home}/>
-        {/* <Route path="/warehouse/:warehouseId" component={Warehouse} /> */}
-        {/* <Route path="/warehouse/:warehouseId/edit" component={WarehouseEdit} /> */}
-        {/* <Route path="/warehouse/add" component={WarehouseAdd} /> */}
-        {/* <Route path="/warehouse/:warehouseId/inventory" component={WarehouseInventory} /> */}
-        {/* <Route path="/warehouse/:warehouseId/inventory/:itemId" component={ItemDetails} /> */}
-        {/* <Route path="/warehouse/:warehouseId/inventory/:itemId/edit" component={ItemEdit} /> */}
-         <Route path="/warehouse/:warehouseId/inventory/add" component={AddnewInventory} />
-      </Switch>
+      <Header />
+      <div className="App">
+        <Switch>
+          <Route path="/" exact component={Home} />
+          {/* <Route path="/warehouse/:warehouseId" component={Warehouse} /> */}
+          {/* <Route path="/warehouse/:warehouseId/edit" component={WarehouseEdit} /> */}
+          {/* <Route path="/warehouse/add" component={WarehouseAdd} /> */}
+          {/* <Route path="/warehouse/:warehouseId/inventory" component={WarehouseInventory} /> */}
+          {/* <Route path="/warehouse/:warehouseId/inventory/:itemId" component={ItemDetails} /> */}
+          {/* <Route path="/warehouse/:warehouseId/inventory/:itemId/edit" component={ItemEdit} /> */}
+          <Route
+            path="/warehouse/:warehouseId/inventory/add"
+            component={AddnewInventory}
+          />
+        </Switch>
+      </div>
     </BrowserRouter>
   );
 }
--- client/src/components/AddNewInventory/AddNewInventory.js
+++ client/src/components/AddNewInventory/AddNewInventory.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import AddNewItemHeader from '../AddNewItemHeader/AddNewItemHeader';
+import './AddNewInventory.scss';
 
 const AddNewInventory = () => {
     return (
-        <div class="new-inventory">
+        <div className="new-inventory">
             <AddNewItemHeader />
         </div>
     );
