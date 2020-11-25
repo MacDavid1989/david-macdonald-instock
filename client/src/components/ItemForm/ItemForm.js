e7cf6361cd4ea73aba358c4267fd66b379e84237
Joseph Abate <Joseph_Abate_@hotmail.com>
2020-11-25 16:59:23 -0500

    half way done form

--- client/src/components/ItemForm/ItemForm.js
+++ client/src/components/ItemForm/ItemForm.js
@@ -1,29 +1,48 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
-import './ItemForm.scss';
+import "./ItemForm.scss";
 
 class ItemForm extends Component {
+  state = {
+    name: "",
+    description: "",
+    category: "",
+    status: true,
+    qty: 0,
+    warehouse: "",
+  };
 
-    state={
-        name: "",
-        description: "",
-        category: "",
-        status: true,
-        qty: 0,
-        warehouse: ""
-    }
+  render() {
+    return (
+      <div className="item-form-com">
+        <h3 className="item-form-com__title">Item Detials</h3>
 
-    render() {
-        return (
-            <div className="item-form-com">
-                <h3 className="item-form-com__title">Item Detials</h3>
+        <form>
+          <div>
+            <label for="name">item Name</label>
+            <input type="text" name="name" />
+          </div>
 
-                <form>
+          <div>
+            <label for="name">Description</label>
+            <textarea></textarea>
+          </div>
 
-                </form>
-            </div>
-        );
-    }
+          <div>
+            <label name="category">Category</label>
+            <select>
+              <option defaultValue hidden>Pick A Category</option>
+              <option value="Accessories">Accessories</option>
+              <option value="Apparel">Apparel</option>
+              <option value="Electronics">Electronics</option>
+              <option value="Gear">Gear</option>
+              <option value="Health">Health</option>
+            </select>
+          </div>
+        </form>
+      </div>
+    );
+  }
 }
 
 export default ItemForm;
