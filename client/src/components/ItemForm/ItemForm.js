e8eb6d54f21bb72001b3cbbcaa0fa3c4705c1f4b
Joseph Abate <Joseph_Abate_@hotmail.com>
2020-11-26 16:20:12 -0500

    added on chnage methods for all inputs

--- client/src/components/ItemForm/ItemForm.js
+++ client/src/components/ItemForm/ItemForm.js
@@ -12,61 +12,138 @@ class ItemForm extends Component {
     warehouse: "",
   };
 
+  checkStatus = (data) =>{
+    if(data="in"){
+      return this.state.status
+    }else{
+      return !this.state.status
+    }
+  }
+
+  onNameChange = (e) => {
+    this.setState({ name: e.target.value });
+  };
+
+  onDescChange = (e) => {
+    this.setState({ description: e.target.value });
+  };
+
+  onCatChange = (e) => {
+    this.setState({ category: e.target.value });
+  };
+
+  onStatusChange = (e) => {
+    this.setState({ status: e.target.value });
+  };
+
+  onQtyChange = (e) => {
+    this.setState({ qty: e.target.value });
+  };
+
+  onWarehouseChange = (e) => {
+    this.setState({ warehouse: e.target.value });
+  };
+
   render() {
     return (
       <div className="item-form-com">
         <form>
-          <div>
+          <section>
             <h3 className="item-form-com__title">Item Detials</h3>
-            <div>
-              <label for="name">item Name</label>
-              <input type="text" name="name" />
-            </div>
+            <div className="item-form-com__wrapper">
+              <div className="item-form-com__wrapper">
+                <label htmlFor="name">Item Name</label>
+                <input
+                  className="item-form-com__input"
+                  type="text"
+                  name="name"
+                  value={this.state.name}
+                  onChange={this.onNameChange}
+                  placeholder="Item Name"
+                />
+              </div>
 
-            <div>
-              <label for="name">Description</label>
-              <textarea></textarea>
-            </div>
+              <div className="item-form-com__wrapper">
+                <label htmlFor="name">Description</label>
+                <textarea
+                  placeholder="Please enter a brief item description..."
+                  className="item-form-com__input item-form-com__input--text-area"
+                  onChange={this.onDescChange}
+                  value={this.state.description}
+                ></textarea>
+              </div>
 
-            <div>
-              <label for="category">Category</label>
-              <select name="category">
-                <option defaultValue hidden>
-                  Pick A Category
-                </option>
-                <option value="Accessories">Accessories</option>
-                <option value="Apparel">Apparel</option>
-                <option value="Electronics">Electronics</option>
-                <option value="Gear">Gear</option>
-                <option value="Health">Health</option>
-              </select>
+              <div className="item-form-com__wrapper">
+                <label htmlFor="category">Category</label>
+                <select
+                  name="category"
+                  value={this.state.category}
+                  onChange={this.onCatChange}
+                  className="item-form-com__input item-form-com__input--select"
+                >
+                  <option defaultValue value="default" hidden>
+                    Please select
+                  </option>
+                  <option value="Accessories">Accessories</option>
+                  <option value="Apparel">Apparel</option>
+                  <option value="Electronics">Electronics</option>
+                  <option value="Gear">Gear</option>
+                  <option value="Health">Health</option>
+                </select>
+              </div>
             </div>
-          </div>
-          <div>
+          </section>
+          <section>
             <h3 className="item-form-com__title">Item Availability</h3>
-            <div>
+            <div className="item-form-com__wrapper">
+              <div className="item-form-com__wrapper">
                 <p>Status</p>
-                <input type="radio" name="status" value={true}/>
-                <label>In Stock</label>
-                <input type="radio" name="status" value={false}/>
-                <label>Out of Stock</label>
+                <div className="item-form-com__wrapper item-form-com__wrapper--radio">
+                  <div className="item-form-com__wrapper--radio-wrapper">
+                    <input
+                      className="item-form-com__radio"
+                      type="radio"
+                      name="status"
+                      value={ this.checkStatus("in")}
+                    />
+                    <label>In Stock</label>
+                  </div>
+                  <div>
+                    <input
+                      className="item-form-com__radio"
+                      type="radio"
+                      name="status"
+                      value={this.checkStatus("in")}
+                    />
+                    <label>Out of Stock</label>
+                  </div>
+                </div>
+              </div>
+              <div className="item-form-com__wrapper">
+                <label htmlFor="qty">Quantity</label>
+                <input
+                  className="item-form-com__input"
+                  type="number"
+                  name="qty"
+                />
+              </div>
+              <div className="item-form-com__wrapper">
+                <label name="warehouse">Warehouse</label>
+                <select
+                  className="item-form-com__input item-form-com__input--select"
+                  name="Warehouse"
+                  onChange={this.onWarehouseChange}
+                >
+                  <option defaultValue hidden>
+                    Please Select
+                  </option>
+                </select>
+              </div>
             </div>
-            <div>
-                <label for="qty">Quantity</label>
-                <input type="number" name="qty" />
-            </div>
-            <div>
-              <label name="warehouse">Warehouse</label>
-              <select name="Warehouse">
-                <option defaultValue hidden>
-                  Please Select
-                </option>
-              </select>
-            </div>
-          </div>
+          </section>
           <div>
-              <button type="reset">Cancel</button>
-              <button type="submit"> + Add Item</button>
+            <button type="reset">Cancel</button>
+            <button type="submit"> + Add Item</button>
           </div>
         </form>
       </div>
