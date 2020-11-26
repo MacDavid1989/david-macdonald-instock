c43b4d9cae0983199335bbce9b9719fcf3da326b
David <62397679+MacDavid1989@users.noreply.github.com>
2020-11-26 13:34:58 -0500

    Build jsx html form structure.

--- client/src/pages/ItemEdit/ItemEdit.js
+++ client/src/pages/ItemEdit/ItemEdit.js
@@ -27,7 +27,48 @@ class ItemEdit extends Component {
                             </Link>
                             <h1 className="banner__heading">Edit Inventory Item</h1>
                     </section>
-                </div> 
+                    <form>
+                        <div>
+                            <h2>Item Details</h2>
+                            <div>
+                                <label>Item Name</label>
+                                <input/>
+                            </div>
+                            <div>
+                                <label>Description</label>
+                                <textarea></textarea>
+                            </div>
+                            <div>
+                                <label>Category</label>
+                                <select>
+                                    <option>Electronics</option>
+                                </select>
+                            </div>
+                        </div>
+                        <div>
+                        <h2>Item Availability</h2>
+                            <div>
+                                <label>Status</label>
+                                <div>
+                                    <div>
+                                        <input type="radio"/>
+                                        <label>In Stock</label>
+                                    </div>
+                                    <div>
+                                        <input type="radio"/>
+                                        <label>Out of Stock</label>
+                                    </div>
+                                </div>
+                            </div>
+                            <div>
+                                <label>Warehouse</label>
+                                <select>
+                                    <option>Manhattan</option>
+                                </select>
+                            </div>
+                        </div>
+                    </form> 
+                </div>
             </>
         );
     }
