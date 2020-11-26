bf84da3928387b5707404488322c366663f1d630
Jasper Leung <JasperLeung@Jaspers-MacBook-Pro.local>
2020-11-26 12:08:17 -0500

    Added WarehouseNav/List components to Home page. Completed Axios request to pull the card info from API

--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -1,11 +1,37 @@
-import React from 'react';
+import WarehouseNav from '../../components/WarehouseNav/WarehouseNav';
+import WarehouseList from '../../components/WarehouseList/WarehouseList';
+import {appUrl} from '../../utils/axios';
+import axios from 'axios';
 
-const Home = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+
+import React, { Component } from 'react';
+
+
+class Home extends Component {
+    state = {
+        warehouses: []
+    }
+
+    componentDidMount() {
+      axios.get(appUrl + "/warehouse")
+      .then((response) => {
+        this.setState({
+          warehouses: response.data,
+        });
+      })
+      .catch((error) => console.log(error));
+    }
+
+    render() {
+        return (
+            <div>
+                <WarehouseNav />
+                <WarehouseList
+                warehouses={this.state.warehouses} 
+                />
+            </div>
+        );
+    }
 }
 
 export default Home;
