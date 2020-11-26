f635dc41e63b73204a059d218ab5635e6d124c1f
David <62397679+MacDavid1989@users.noreply.github.com>
2020-11-26 11:42:34 -0500

    Edit styling to match mockup at mobile

--- client/src/pages/ItemDetails/ItemDetails.js
+++ client/src/pages/ItemDetails/ItemDetails.js
@@ -22,8 +22,8 @@ class ItemDetails extends Component {
     render() {
         return (
             <>
-                <div className="item">
-                    <section className="item__banner">
+                <div className="itemDetails">
+                    <section className="itemDetails__banner">
                         <div className="banner__header">
                             <Link to={`/warehouse/${this.props.match.params.warehouseId}/inventory`}>
                                 <img className="banner__arrow" src={backArrow} alt=""/>
@@ -37,30 +37,30 @@ class ItemDetails extends Component {
                             </button>
                         </Link>
                     </section>
-                    <section className="item__details">
-                        <div className="item__about">
-                            <div className="item__description">
+                    <section className="itemDetails__details">
+                        <div className="itemDetails__about">
+                            <div className="itemDetails__description">
                                 <h4 className="description__heading">ITEM DESCRIPTION:</h4>
                                 <p className="description__text">{!!this.state.item&&this.state.item.description}</p>
                             </div>
-                            <div className="item__category">
+                            <div className="itemDetails__category">
                                 <h4 className="category__heading">CATEGORY:</h4>
                                 <p className="category__text">{!!this.state.item&&this.state.item.category}</p>
                             </div>
                         </div>
-                        <div className="item__info">
-                            <div className="item__stock">
-                                <div className="item__status">
+                        <div className="itemDetails__info">
+                            <div className="itemDetails__stock">
+                                <div className="itemDetails__status">
                                     <h4 className="status__heading">STATUS:</h4>
                                     <p className={!!this.state.item&&this.state.item.status.toUpperCase() === 'IN STOCK' ? "status__value status__value--in" : "status__value status__value--out"}>{!!this.state.item&&this.state.item.status}</p>
                                 </div>
-                                <div className="item__quantity">
+                                <div className="itemDetails__quantity">
                                     <h4 className="quantity__heading">QUANTITY:</h4>
                                     <p className="quantity__text">{!!this.state.item&&this.state.item.quantity}</p>
                                 </div>
                             </div>
                             <div>
-                            <div className="item__warehouse">
+                            <div className="itemDetails__warehouse">
                                     <h4 className="warehouse__heading">WAREHOUSE:</h4>
                                     <p className="warehouse__text">{!!this.state.item&&this.state.item.warehouseName}</p>
                                 </div>
