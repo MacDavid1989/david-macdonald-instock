d8859dd0fc56a7ea4138bedd63fee3c92a76c901
Joseph Abate <Joseph_Abate_@hotmail.com>
2020-11-26 15:25:11 -0500

    fixed routes

--- client/src/App.js
+++ client/src/App.js
@@ -3,29 +3,36 @@ import { BrowserRouter, Switch, Route } from "react-router-dom";
 import Home from "./pages/Home/Home";
 import Header from "./components/Header/Header";
 import AddnewInventory from "./components/AddNewInventory/AddNewInventory";
-import Warehouse from './pages/Warehouse/Warehouse';
-import ItemDetails from './pages/ItemDetails/ItemDetails';
-import ItemEdit from './pages/ItemEdit/ItemEdit';
+import Warehouse from "./pages/Warehouse/Warehouse";
+import ItemDetails from "./pages/ItemDetails/ItemDetails";
+import ItemEdit from "./pages/ItemEdit/ItemEdit";
 
-import './App.scss';
-
+import "./App.scss";
 
 function App() {
   return (
     <BrowserRouter>
-    <Header />
+      <Header />
       <Switch>
-        <Route path="/" exact component={Home}/>
+        <Route path="/" exact component={Home} />
         {/* <Route path="/warehouse/:warehouseId/edit" component={WarehouseEdit} /> */}
         {/* <Route path="/warehouse/add" component={WarehouseAdd} /> */}
         {/* <Route path="/warehouse/:warehouseId/inventory" component={WarehouseInventory} /> */}
-        <Route path="/warehouse/:warehouseId/inventory/:itemId/edit" component={ItemEdit} />
-        <Route path="/warehouse/:warehouseId/inventory/:itemId" component={ItemDetails} />
-        <Route path="/warehouse/:warehouseId" component={Warehouse} />
         <Route
-            path="/warehouse/:warehouseId/inventory/add"
-            component={AddnewInventory}
-          />
+          exact
+          path="/warehouse/:warehouseId/inventory/:itemId/edit"
+          component={ItemEdit}
+        />
+        <Route
+          path="/warehouse/:warehouseId/inventory/add"
+          component={AddnewInventory}
+        />
+        <Route
+          exact
+          path="/warehouse/:warehouseId/inventory/:itemId"
+          component={ItemDetails}
+        />
+        <Route exact path="/warehouse/:warehouseId" component={Warehouse} />
       </Switch>
     </BrowserRouter>
   );
