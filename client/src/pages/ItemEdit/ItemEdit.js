ab2164dfc3763da78a5a7315e4f6c0e60ca80bc9
David <62397679+MacDavid1989@users.noreply.github.com>
2020-11-26 13:13:53 -0500

    Create itemedit component. Provide state and did mount method to get item details. Build JSX html structure for banner.

--- client/src/pages/ItemEdit/ItemEdit.js
+++ client/src/pages/ItemEdit/ItemEdit.js
@@ -1,11 +1,34 @@
 import React, { Component } from 'react';
-
+import axios from 'axios';
+import {Link} from 'react-router-dom';
+import './ItemEdit.scss'
+import backArrow from '../../Assets/Icons/arrow_back-24px.svg'
 class ItemEdit extends Component {
+    state ={
+        item: null
+    }
+
+    componentDidMount(){
+
+        axios.get(`http://localhost:8080/warehouse/${this.props.match.params.warehouseId}/inventory/${this.props.match.params.itemId}`)
+        .then(res=>{
+            this.setState({item: res.data.inventoryItem[0]})
+        })
+        .catch(console.error)
+    }
+
     render() {
         return (
-            <div>
-                
-            </div>
+            <>
+               <div className="itemEdit">
+                    <section className="itemEdit__banner">
+                            <Link to={`/warehouse/${this.props.match.params.warehouseId}/inventory/${this.props.match.params.itemId}`}>
+                                <img className="banner__arrow" src={backArrow} alt=""/>
+                            </Link>
+                            <h1 className="banner__heading">{!!this.state.item&&this.state.item.itemName}</h1>
+                    </section>
+                </div> 
+            </>
         );
     }
 }
