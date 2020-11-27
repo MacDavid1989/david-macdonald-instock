562d63294ae7bbdcc63ddc7e76067e9a167526a1
Joseph Abate <Joseph_Abate_@hotmail.com>
2020-11-27 17:50:14 -0500

    upodated style for tablet

--- client/src/components/DeleteModal/DeleteModal.js
+++ client/src/components/DeleteModal/DeleteModal.js
@@ -4,7 +4,6 @@ import close from "../../assets/icons/close-24px.svg";
 import axios from "axios";
 import { withRouter } from "react-router-dom";
 
-
 class DeleteModal extends Component {
   constructor(props) {
     super(props);
@@ -23,26 +22,27 @@ class DeleteModal extends Component {
 
   //delete the item
   onSubmitDelete = () => {
-      axios.delete(`http://localhost:8080/warehouse/${this.props.deleteThing.id}`)
-      .then((res)=>{
-          console.log(res.data.warehouses);
+    axios
+      .delete(`http://localhost:8080/warehouse/${this.props.deleteThing.id}`)
+      .then((res) => {
+        console.log(res.data.warehouses);
         this.props.updateWarehouse(res.data.warehouses);
-      })
+      });
+  };
+
+  componentDidMount() {
+    if (this.props.deleteThing.name) {
+      this.setState({
+        display: true,
+      });
+    }
   }
 
-  componentDidMount(){
-      if(this.props.deleteThing.name){
-          this.setState({
-              display: true
-          });
-      }
-  }
-
-  componentDidUpdate(prevP, prevS){
-    if(prevS.display == this.state.display && this.props.deleteThing.name){
-        this.setState({
-            display: true
-        });
+  componentDidUpdate(prevP, prevS) {
+    if (prevS.display == this.state.display && this.props.deleteThing.name) {
+      this.setState({
+        display: true,
+      });
     }
   }
 
@@ -53,7 +53,9 @@ class DeleteModal extends Component {
         style={{ display: this.state.display ? "block" : "none" }}
       >
         <div className="deleteModal__wrapper">
-          <img src={close} alt="close" onClick={this.onCloseDeleteScreen} />
+          <div className="deleteModal__close-wrapper">
+            <img src={close} alt="close" onClick={this.onCloseDeleteScreen} />
+          </div>
           <div>
             <h1>Delete {this.props.deleteThing.name} warehouse?</h1>
             <p>
@@ -61,9 +63,17 @@ class DeleteModal extends Component {
               list of warehouses. You won't be able to undo this action.
             </p>
           </div>
-          <div>
-            <button className="deleteModal__btn" onClick={this.onCloseDeleteScreen}>Cancel</button>
-            <button className="deleteModal__btn deleteModal__btn--delete" onClick={this.onSubmitDelete}>
+          <div className="deleteModal__btn-wrapper">
+            <button
+              className="deleteModal__btn"
+              onClick={this.onCloseDeleteScreen}
+            >
+              Cancel
+            </button>
+            <button
+              className="deleteModal__btn deleteModal__btn--delete"
+              onClick={this.onSubmitDelete}
+            >
               Delete
             </button>
           </div>
