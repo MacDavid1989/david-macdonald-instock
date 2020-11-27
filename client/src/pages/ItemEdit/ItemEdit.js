f0909d357904f267e049cf1725d1abe2257326a9
David <62397679+MacDavid1989@users.noreply.github.com>
2020-11-26 21:04:57 -0500

    Add onChange handlers for each input to change state based on input value. Create getItem function for get axios request. Create getAllWarehouses for get axios request. Use onClick handler for reset to make another request to get current item data and set state with original values. Create form handler to generate updated item and make put request. Upon successfull put the page will direct to the item in whichever warehouse it belongs to.

--- client/src/pages/ItemEdit/ItemEdit.js
+++ client/src/pages/ItemEdit/ItemEdit.js
@@ -4,27 +4,121 @@ import {Link} from 'react-router-dom';
 import './ItemEdit.scss'
 import backArrow from '../../Assets/Icons/arrow_back-24px.svg'
 class ItemEdit extends Component {
-    state ={
-        item: null,
-        category: ["Accessories", "Apparel", "Electronics", "Gear", "Health"],
-        warehouse: null
+    state = {
+        name: "",
+        description: "",
+        category: "",
+        categoryList: ["Accessories", "Apparel", "Electronics", "Gear", "Health"],
+        status: "",
+        check: true,
+        quantity: "",
+        warehouse: "",
+        warehouseList: [],
+        warehouseNameList: []
+    }
+
+    getItem = () => {
+        axios.get(`http://localhost:8080/warehouse/${this.props.match.params.warehouseId}/inventory/${this.props.match.params.itemId}`)
+        .then(res=>{
+            this.setState({
+                name: res.data.inventoryItem[0].itemName,
+                description: res.data.inventoryItem[0].description,
+                category: res.data.inventoryItem[0].category,
+                status: res.data.inventoryItem[0].status,
+                quantity: res.data.inventoryItem[0].quantity,
+                warehouse: res.data.inventoryItem[0].warehouseName,
+            })
+            if(res.data.inventoryItem[0].status.toLowerCase() === 'out of stock'){
+                this.setState({
+                    check: false,
+                })
+            } else {
+                this.setState({
+                    check: true,
+                })
+            }
+        })
+        .catch(console.error)
+    }
+
+    getAllWarehouses = () => {
+        axios.get(`http://localhost:8080/warehouse`)
+        .then(res=>{
+            this.setState({
+                warehouseNameList: res.data.map(warehouse=> warehouse.name).sort(),
+                warehouseList: res.data
+            })
+        })
+        .catch(console.error)
     }
 
     componentDidMount(){
-
-        axios.get(`http://localhost:8080/warehouse/${this.props.match.params.warehouseId}/inventory/${this.props.match.params.itemId}`)
-        .then(res=>{
-            this.setState({item: res.data.inventoryItem[0]})
-        })
-        .catch(console.error)
-
-        axios.get(`http://localhost:8080/warehouse`)
-        .then(res=>{
-            this.setState({warehouse: res.data.map(warehouse=> warehouse.name).sort()})
-        })
-        .catch(console.error)
-
+        this.getItem();
+        this.getAllWarehouses();
     }
+
+    handleName = (e) => {
+        this.setState({ 
+            name: e.target.value 
+        });
+    };
+    
+    handleDescription = (e) => {
+        this.setState({ 
+            description: e.target.value 
+        });
+    };
+    
+    handleCategory = (e) => {
+        this.setState({ 
+            category: e.target.value 
+        });
+    };
+    
+    handleStatus = (e) => {
+        const bool = (e.target.value === "true")
+        if(bool){
+            this.setState({status: "In Stock"})
+        } else {
+            this.setState({status: "Out of Stock"})
+        }
+        this.setState({
+          check: bool
+        });
+    };
+    
+    handleWarehouse = (e) => {
+        this.setState({ warehouse: e.target.value });
+    };
+
+    handleReset = () => {
+        this.getItem()
+    }
+    
+    handleSubmit = (e) => {
+        e.preventDefault();
+
+        const warehouseID = this.state.warehouseList.find(warehouse => warehouse.name === this.state.warehouse).id
+
+        const item = {
+            category: this.state.category,
+            description: this.state.description,
+            id: this.props.match.params.itemId,
+            itemName: this.state.name,
+            quantity: this.state.quantity,
+            status: this.state.status,
+            warehouseID: warehouseID,
+            warehouseName: this.state.warehouse
+        }
+
+        // axios.put(`http://localhost:8080/warehouse/${this.props.match.params.warehouseId}/inventory/${this.props.match.params.itemId}/edit`, item)
+        // .then(res => {
+        //     this.props.history.push(`http://localhost:8080/warehouse/${this.props.match.params.warehouseId}/inventory/${this.props.match.params.itemId}`)
+        // })
+        // .catch(console.error)
+
+        this.props.history.push(`http://localhost:8080/warehouse/${warehouseID}/inventory/${this.props.match.params.itemId}`)
+    };
 
     render() {
         return (
@@ -36,22 +130,22 @@ class ItemEdit extends Component {
                             </Link>
                             <h1 className="banner__heading">Edit Inventory Item</h1>
                     </section>
-                    <form className="itemEdit__form">
+                    <form className="itemEdit__form" onSubmit={this.handleSubmit}>
                         <div className="itemEdit__details">
                             <h2 className="itemEdit__details-title">Item Details</h2>
                             <div className="itemEdit__details-name">
                                 <label className="itemEdit__details-label">Item Name</label>
-                                <input id="itemName" name="itemName" type="text" placeholder={`${!!this.state.item&&this.state.item.itemName}`}/>
+                                <input id="itemName" name="itemName" type="text" placeholder="Item Name" value={this.state.name} onChange={this.handleName}/>
                             </div>
                             <div className="itemEdit__details-description">
                                 <label className="itemEdit__details-label">Description</label>
-                                <textarea id="itemDescription" name="itemDescription" placeholder={`${!!this.state.item&&this.state.item.description}`}></textarea>
+                                <textarea id="itemDescription" name="itemDescription" placeholder="Description" value={this.state.description} onChange={this.handleDescription}></textarea>
                             </div>
                             <div className="itemEdit__details-category">
                                 <label className="itemEdit__details-label">Category</label>
-                                <select id="itemCategory" name="itemCategory">
-                                {this.state.category.map((category, i) => (
-                                    <option key={i}>{category}</option>
+                                <select id="itemCategory" name="itemCategory" value={this.state.category} onChange={this.handleCategory}>
+                                {this.state.categoryList.map((category, i) => (
+                                    <option key={i} value={category}>{category}</option>
                                 ))}
                                 </select>
                             </div>
@@ -62,26 +156,26 @@ class ItemEdit extends Component {
                                 <label className="itemEdit__availability-label">Status</label>
                                 <div className="status__wrapper">
                                     <div className="status__in">
-                                        <input id="itemStatus" name="itemStatus" type="radio"/>
+                                        <input id="itemStatus" name="itemStatus" type="radio" checked={this.state.check} value="true" onChange={this.handleStatus}/>
                                         <label className={!!this.state.item&&this.state.item.status.toLowerCase() === "in stock" ? "status__in-radio checked" : "status__in-radio"}>In Stock</label>
                                     </div>
                                     <div className="status__out">
-                                        <input id="itemStatus" name="itemStatus" type="radio"/>
+                                        <input id="itemStatus" name="itemStatus" type="radio" checked={!this.state.check} value="false" onChange={this.handleStatus}/>
                                         <label className={!!this.state.item&&this.state.item.status.toLowerCase() === "out of stock" ? "status__out-radio checked" : "status__out-radio"}>Out of Stock</label>
                                     </div>
                                 </div>
                             </div>
                             <div className="itemEdit__availability-warehouse">
                                 <label className="itemEdit__availability-label">Warehouse</label>
-                                <select id="itemWarehouse" name="itemWarehouse">
-                                {!!this.state.warehouse&&this.state.warehouse.map((warehouse, i) => (
-                                    <option key={i}>{warehouse}</option>
+                                <select id="itemWarehouse" name="itemWarehouse" value={this.state.warehouse} onChange={this.handleWarehouse}>
+                                {this.state.warehouseNameList.map((warehouse, i) => (
+                                    <option key={i} value={warehouse}>{warehouse}</option>
                                 ))}
                                 </select>
                             </div>
                         </div>
                         <div className="itemEdit__buttons">
-                            <button className="itemEdit__cancel" type="reset">Cancel</button>
+                            <button className="itemEdit__cancel" type="reset" onClick={this.handleReset}>Cancel</button>
                             <button className="itemEdit__save" type="submit">Save</button>
                         </div>
                     </form> 
