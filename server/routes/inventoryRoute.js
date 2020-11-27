a160cb8b407c0d6a006b46faa41a2997a7ea1152
henna fatemah <hennafatemah@hennas-MacBook-Air.local>
2020-11-27 13:13:45 -0500

    comment corrected

--- server/routes/inventoryRoute.js
+++ server/routes/inventoryRoute.js
@@ -13,7 +13,7 @@ router.get("/", (req, res) => {
     res.status(200).json(data)
 })
 
-// <<<<<<<<<< POST /videos >>>>>>>>>> \\
+// <<<<<<<<<< POST >>>>>>>>>> \\
 
 router.post ('/', inventoryController.createNewInventory);
 
