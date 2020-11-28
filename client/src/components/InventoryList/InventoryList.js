8cc38959776dd89a098ca279176df4ea8741a09e
Jasper Leung <JasperLeung@Jaspers-MacBook-Pro.local>
2020-11-28 00:22:07 -0500

    Mobile scss complete

--- client/src/components/InventoryList/InventoryList.js
+++ client/src/components/InventoryList/InventoryList.js
@@ -7,40 +7,42 @@ import './InventoryList.scss'
 const InventoryList = (props) => {
     const inventories = props.inventories && props.inventories.map(item => {
         return(
-            <div className="card" key={item.id}>
-                <div className="card__top-container">
-                    <div className="card__item-category">
-                        <div className="card__inventory-item">
-                            <span className="card__subheader">INVENTORY ITEM</span>
-                            <p className="card__item-container">
-                                <span className="card__stock-item">{item.itemName}</span>
+            <div className="inventory-card" key={item.id}>
+                <div className="inventory-card__top-container">
+                    <div className="inventory-card__item-category">
+                        <div className="inventory-card__inventory-item">
+                            <span className="inventory-card__subheader">INVENTORY ITEM</span>
+                            <p className="inventory-card__item-container">
+                                <span className="inventory-card__stock-item">{item.itemName}</span>
                                 <span>
-                                    <img src={chevronRight} className="card__chevron"alt="chevronRight"/>
+                                    <img src={chevronRight} className="inventory-card__chevron"alt="chevronRight"/>
                                 </span>
                             </p>
                         </div>
-                        <div className="card__category">
-                            <span className="card__subheader">CATEGORY</span>
-                            <span className="card__info>">{item.category}</span>
+                        <div className="inventory-card__category">
+                            <span className="inventory-card__subheader">CATEGORY</span>
+                            <span className="inventory-card__info">{item.category}</span>
                         </div>
                     </div>
-                    <div className="card__status-qty-warehouse">
-                        <div className="card__status">
-                            <span className="card__subheader">STATUS</span>
-                            <span className="card__stock">{item.status}</span>
+                    <div className="inventory-card__status-qty-warehouse">
+                        <div className="inventory-card__status">
+                            <span className="inventory-card__subheader">STATUS</span>
+                           {item.status === "In Stock" && <span className="inventory-card__stock inventory-card__stock--in">{item.status}</span>}
+                           {item.status === "Out of Stock" && <span className="inventory-card__stock inventory-card__stock--out">{item.status}</span>}
                         </div>
-                        <div className="card__quantity">
-                            <span className="card__subheader">QTY</span>
-                            <span className="card__info">{item.quantity}</span>
+                        <div className="inventory-card__quantity">
+                            <span className="inventory-card__subheader">QTY</span>
+                            <span className="inventory-card__info">{item.quantity}</span>
                         </div>
-                        <div className="card__location-container">
-                            <span className="card__location">{item.warehouseName}</span>
+                        <div className="inventory-card__location-container">
+                            <span className="inventory-card__subheader">WAREHOUSE</span>
+                            <span className="inventory-card__info">{item.warehouseName}</span>
                         </div>
                     </div>
                 </div>
-                <div className="card__icons">
-                    <img src={deleteOutline} alt="delete" className="card__delete-icon"/>
-                    <img src={edit} alt="edit" className="card__edit-icon"/>
+                <div className="inventory-card__icons">
+                    <img src={deleteOutline} alt="delete" className="inventory-card__delete-icon"/>
+                    <img src={edit} alt="edit" className="inventory-card__edit-icon"/>
                 </div>
             </div>)
     })
