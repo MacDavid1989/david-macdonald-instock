656c9bbdf6df4169df5b42d141409f8d08568971
David <62397679+MacDavid1989@users.noreply.github.com>
2020-11-28 12:36:54 -0500

    update form values and placeholders

--- client/src/pages/WarehouseEdit/WarehouseEdit.js
+++ client/src/pages/WarehouseEdit/WarehouseEdit.js
@@ -122,32 +122,32 @@ class EditWarehouse extends Component {
 
                                     <h3 className="edit-warehouse__details-title">warehouse details</h3>
                                     <label htmlFor="warehouseName" className="edit-warehouse__tag" >warehouse name</label>
-                                    <input name= "warehouseName" placeholder="King West" type="text" className="edit-warehouse__insert" value={this.state.warehouseName} onChange={this.onNameChange} />
+                                    <input name= "warehouseName" placeholder="Warehouse name" type="text" className="edit-warehouse__insert" value={this.state.name} onChange={this.onNameChange} />
 
                                     <label htmlFor="address" className="edit-warehouse__tag">street address</label>
-                                    <input name= "address" placeholder="469 King Street West" type="address" className="edit-warehouse__insert" value={this.state.address} onChange={this.onAddressChange} />
+                                    <input name= "address" placeholder="Warehouse address" type="address" className="edit-warehouse__insert" value={this.state.address} onChange={this.onAddressChange} />
 
                                     <label htmlFor="city" className="edit-warehouse__tag">city</label>
-                                    <input name= "city" placeholder="Toronto" type="text" className="edit-warehouse__insert" value={this.state.city} onChange={this.onCityChange} />
+                                    <input name= "city" placeholder="Warehouse city" type="text" className="edit-warehouse__insert" value={this.state.city} onChange={this.onCityChange} />
 
                                     <label htmlFor="country" className="edit-warehouse__tag">country</label>
-                                    <input name= "country" placeholder="CAN" type="text" className="edit-warehouse__insert" value={this.state.country} onChange={this.onCountryChange} />
+                                    <input name= "country" placeholder="Warehouse country" type="text" className="edit-warehouse__insert" value={this.state.country} onChange={this.onCountryChange} />
                                 </div>
 
                                 {/*  <<<<<<<<<< CONTACT DETAILS >>>>>>>>>> */}
                                 <div className="edit-warehouse__contact-details">
                                     <h3 className="edit-warehouse__details-title">contact details</h3>
                                     <label htmlFor="contactName" className="edit-warehouse__tag">contact name</label>
-                                    <input name= "contactName" placeholder="I" type="text"  className="edit-warehouse__insert" value={this.state.contactName} onChange={this.onContactNameChange} />
+                                    <input name= "contactName" placeholder="Contact name" type="text"  className="edit-warehouse__insert" value={this.state.contact.name} onChange={this.onContactNameChange} />
 
                                     <label htmlFor="position" className="edit-warehouse__tag">position</label>
-                                    <input name= "position" placeholder="Position" type="text" className="edit-warehouse__insert" value={this.state.position} onChange={this.onPositionChange} />
+                                    <input name= "position" placeholder="Contact position" type="text" className="edit-warehouse__insert" value={this.state.contact.position} onChange={this.onPositionChange} />
 
                                     <label htmlFor="phone"  className="edit-warehouse__tag">phone number</label>
-                                    <input name= "phone" placeholder="Phone Number" type="tel"  className="edit-warehouse__insert" value={this.state.phone} onChange={this.onPhoneChange} />
+                                    <input name= "phone" placeholder="Contact number" type="tel"  className="edit-warehouse__insert" value={this.state.contact.phone} onChange={this.onPhoneChange} />
 
                                     <label htmlFor="email"  className="edit-warehouse__tag">email</label>
-                                    <input name= "email" placeholder="Email" type="email" className="edit-warehouse__insert" value={this.state.email} onChange={this.onEmailChange} />
+                                    <input name= "email" placeholder="Contact email" type="email" className="edit-warehouse__insert" value={this.state.contact.email} onChange={this.onEmailChange} />
                                 </div>
                             </div>
                             {/*  <<<<<<<<<< EDIT WAREHOUSE FOOTER >>>>>>>>>> */}
