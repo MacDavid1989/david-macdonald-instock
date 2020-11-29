dc7b0214ae3773ab834bdde155dc02bdb19624b8
David <62397679+MacDavid1989@users.noreply.github.com>
2020-11-28 21:31:09 -0500

    Merge conflict issue fixed so develop npm start works

--- client/src/components/WarehouseList/WarehouseList.js
+++ client/src/components/WarehouseList/WarehouseList.js
@@ -10,11 +10,48 @@ const WarehouseList = ({warehouses}) => {
     const showWarehouses = warehouses.map((warehouse) => {
         return(
             <>
-        <div className="card card-mob" key={warehouse.id}>
-            <div className="card__top-container">
-                <div className="card__warehouse-address">
-                    <div className="card__warehouse">
-                        <span className="card__subheader">WAREHOUSE</span>
+                <div className="card card-mob" key={warehouse.id}>
+                    <div className="card__top-container">
+                        <div className="card__warehouse-address">
+                            <div className="card__warehouse">
+                                <span className="card__subheader">WAREHOUSE</span>
+                                <p className="card__location-container">
+                                    <Link to={`/warehouse/${warehouse.id}`} className="card__location">
+                                        <span className="card__location">{warehouse.name}</span>
+                                        <span>
+                                            <img src={chevronRight} className="card__chevron"alt="chevronRight"/>
+                                        </span>
+                                    </Link>
+                                </p>
+                            </div>
+                            <div className="card__address">
+                                <span className="card__subheader">ADDRESS</span>
+                                <span className="card__info card__info--address">{`${warehouse.address}, ${warehouse.city}, ${warehouse.country}`}</span>
+                            </div>
+                        </div>
+                        <div className="card__contact-details">
+                            <div className="card__name">
+                                <span className="card__subheader">CONTACT NAME</span>
+                                <span className="card__info card__info--name">{warehouse.contact.name}</span>
+                            </div>
+                            <div className="card__contact-info">
+                                <span className="card__subheader">CONTACT INFORMATION</span>
+                                <div className="card__phone-email">
+                                    <span className="card__info card__phone">{warehouse.contact.phone}</span>
+                                    <span className="card__info card__email">{warehouse.contact.email}</span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="card__icons">
+                            <img src={deleteOutline} alt="delete" className="card__delete-icon"/>
+                        <Link to={`/warehouse/${warehouse.id}/edit`}>
+                            <img src={edit} alt="edit" className="card__edit-icon"/>
+                        </Link>
+                    </div>
+                </div>
+                <div className="card card-tab" key={warehouse.id}>
+                    <div className="card__top-container">
                         <p className="card__location-container">
                             <Link to={`/warehouse/${warehouse.id}`} className="card__location">
                                 <span className="card__location">{warehouse.name}</span>
@@ -23,95 +60,33 @@ const WarehouseList = ({warehouses}) => {
                                 </span>
                             </Link>
                         </p>
-                    </div>
-                    <div className="card__address">
-                        <span className="card__subheader">ADDRESS</span>
-                        <span className="card__info card__info--address">{`${warehouse.address}, ${warehouse.city}, ${warehouse.country}`}</span>
-                    </div>
-                </div>
-                <div className="card__contact-details">
-                    <div className="card__name">
-                        <span className="card__subheader">CONTACT NAME</span>
-                        <span className="card__info card__info--name">{warehouse.contact.name}</span>
-                    </div>
-                    <div className="card__contact-info">
-                        <span className="card__subheader">CONTACT INFORMATION</span>
+                        <div className="card__address">
+                            <span className="card__info card__info--address">{`${warehouse.address}, ${warehouse.city}, ${warehouse.country}`}</span>
+                        </div>
+                        <div className="card__name">
+                            <span className="card__info card__info--name">{warehouse.contact.name}</span>
+                        </div>
                         <div className="card__phone-email">
                             <span className="card__info card__phone">{warehouse.contact.phone}</span>
                             <span className="card__info card__email">{warehouse.contact.email}</span>
                         </div>
-                    </div>
-                </div>
-            </div>
-            <div className="card__icons">
-                    <img src={deleteOutline} alt="delete" className="card__delete-icon"/>
-                <Link to={`/warehouse/${warehouse.id}/edit`}>
-                    <img src={edit} alt="edit" className="card__edit-icon"/>
-
-                </Link>
-              </p>
-            </div>
-
-        </div>
-
-
-
-        <div className="card card-tab" key={warehouse.id}>
-            <div className="card__top-container">
-            <p className="card__location-container">
-                            <Link to={`/warehouse/${warehouse.id}`} className="card__location">
-                                <span className="card__location">{warehouse.name}</span>
-                                <span>
-                                    <img src={chevronRight} className="card__chevron"alt="chevronRight"/>
-                                </span>
+                        <div className="card__icons">
+                            <img src={deleteOutline} alt="delete" className="card__delete-icon"/>
+                            <Link to={`/warehouse/${warehouse.id}/edit`}>
+                                <img src={edit} alt="edit" className="card__edit-icon"/>
                             </Link>
-                        </p>
-                <div className="card__address">
-                    <span className="card__info card__info--address">{`${warehouse.address}, ${warehouse.city}, ${warehouse.country}`}</span>
+                        </div>
+                    </div>  
                 </div>
-                <div className="card__name">
-                    <span className="card__info card__info--name">{warehouse.contact.name}</span>
-                </div>
-                <div className="card__phone-email">
-                    <span className="card__info card__phone">{warehouse.contact.phone}</span>
-                    <span className="card__info card__email">{warehouse.contact.email}</span>
-                </div>
-                <div className="card__icons">
-                <img src={deleteOutline} alt="delete" className="card__delete-icon"/>
-                <Link to={`/warehouse/${warehouse.id}/edit`}>
-                    <img src={edit} alt="edit" className="card__edit-icon"/>
-                </Link>
-            </div>
-            </div>
-            
-        </div>
-        
-    </>
+            </>
         )
     })
+
     return (
-        <div>
+        <div className="">
             {showWarehouses}
-
         </div>
-        <div className="card__icons">
-          <img
-            src={deleteOutline}
-            alt="delete"
-            className="card__delete-icon"
-            onClick={() => {
-              setDelete(warehouse.id, warehouse.name);
-            }}
-          />
-          <Link to={`/warehouse/${warehouse.id}/edit`}>
-            <img src={edit} alt="edit" className="card__edit-icon" />
-          </Link>
-        </div>
-      </div>
     );
-  });
-
-  return <div>{showWarehouses}</div>;
 };
 
 export default WarehouseList;
