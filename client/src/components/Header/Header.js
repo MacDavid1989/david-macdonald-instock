d3567a7ef967e4c6b96d1c1ebf96fd4051533cf6
David <62397679+MacDavid1989@users.noreply.github.com>
2020-11-29 18:14:24 -0500

    Header scss complete

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -7,21 +7,23 @@ import {Link, NavLink } from 'react-router-dom';
 const header = () => {
   return (
     <header className="header">
-        <Link to="/">
-          <img className="header__logo" src={logo} alt="logo" />
-        </Link>
-      <div className="header__wrapper">
-        <NavLink to="/warehouse" className="header__btn"
-        activeStyle={{
-          color: '#FFFFFF',
-          backgroundColor: "#13182C"
-        }} >Warehouses</NavLink>
-        <NavLink to="/inventory" className="header__btn"
-        activeStyle= {{
-          color: '#FFFFFF',
-          backgroundColor: "#13182C"
-        }} >Inventory</NavLink>
-      </div>
+        <div className="header__container">
+          <Link to="/">
+            <img className="header__logo" src={logo} alt="logo" />
+          </Link>
+          <div className="header__wrapper">
+            <NavLink to="/warehouse" className="header__btn"
+            activeStyle={{
+            color: '#FFFFFF',
+            backgroundColor: "#13182C"
+            }} >Warehouses</NavLink>
+            <NavLink to="/inventory" className="header__btn"
+            activeStyle= {{
+            color: '#FFFFFF',
+            backgroundColor: "#13182C"
+            }} >Inventory</NavLink>
+          </div>
+        </div>
     </header>
   );
 };
